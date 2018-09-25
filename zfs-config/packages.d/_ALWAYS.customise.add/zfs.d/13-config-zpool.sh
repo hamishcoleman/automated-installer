b7223cbbca4df7cebf418bf804d90d969b76f4a5
Hamish Coleman <hamish@zot.org>
2018-09-25 10:18:52 +1000

    Add some ZFS tuning notes for future consideration

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/13-config-zpool.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/13-config-zpool.sh
@@ -4,6 +4,13 @@
 
 ZFS_POOL_SCRIPT=/tmp/zfs.zpool.script
 
+# TODO
+# Some notes from http://jrs-s.net/2018/08/17/zfs-tuning-cheat-sheet/
+# - ashift, get it wrong on the high side 12==4K sectors
+# - xattr=sa, make xattrs be in the inode, instead of a separate file(s)
+# - recordsize=16K, probably better than the default of 128K
+# - l2arc: "nope!"
+
 cat <<EOF >"$ZFS_POOL_SCRIPT"
 # The command to use to assemble the ZFS filesystem.  For review and editing
 zpool create -f \\
